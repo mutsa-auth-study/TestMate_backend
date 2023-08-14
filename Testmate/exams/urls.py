0946412bed64ce793f65b07b6abb585ccf97a136
jiwoong264 <jiwoong264@gmail.com>
2023-08-15 06:49:10 +0900

    Feat: 시험 상세 조회 API 구현. 로그인 후 최근시험 등록 확인 필요

--- Testmate/exams/urls.py
+++ Testmate/exams/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from .views import ExamList, ExamPlan, RecentExamListView
+from .views import ExamList, ExamDetail, RecentExamListView
 #from .views import setExamDB_XML, deleteExamDB_XML
 
 urlpatterns = [
     path("", ExamList.as_view(), name='getExamList'),
-    path("<int:exam_id>/", ExamPlan.as_view(), name='getExamPlan'),
+    path("<str:exam_id>/", ExamDetail.as_view(), name='getExamDetail'),
     path("recent/", RecentExamListView.as_view(), name='getRecentExam'),
 
     # path("setDB/", setExamDB.as_view(), name='setDB'),
