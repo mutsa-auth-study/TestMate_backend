f6283f12e693c2f9a278399006ec61a9b5ddf959
soyeoneeii <syp0463@gmail.com>
2023-08-15 03:25:48 +0900

    exams:recent url 연결

--- Testmate/exams/urls.py
+++ Testmate/exams/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-# from .views import ExamDetailAPIMixins, ExamTotalAPIMixins
 
-from .views import ExamDetail
+from .views import ExamDetail,RecentExamListView
 
 urlpatterns = [
-    #path("", ExamTotalAPIMixins.as_view()),
-    # path("<int:exam_id>/", ExamDetailAPIMixins.as_view()),
     path("<int:exam_id>/", ExamDetail.as_view(), name='getExamPlan'),
+    path("recent/", RecentExamListView.as_view(), name='getRecentExam'),
 ]
