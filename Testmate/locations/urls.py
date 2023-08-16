fb9ce8a27427c1dfdbe7b14afafbc4cc2d172acb
jiwoong264 <jiwoong264@gmail.com>
2023-08-17 00:10:38 +0900

    Fix: location DB 관련 view, url 주석처리

--- Testmate/locations/urls.py
+++ Testmate/locations/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("comment/<uuid:location_id>/", getLocationComment.as_view(), name="LocationCommentList"), # 고사장 리뷰 조회 [GET]
     # path('comment/', MainLocationCommentView.as_view(), name='locationComment'), # 고사장 리뷰[POST, PATCH, DELETE]
 
-    path("setDB/", setLocationDB.as_view(), name='setLocationDB'),
-    path("delDB/", deleteLocationDB.as_view(), name='delLocationDB'),
+    # path("setDB/", setLocationDB.as_view(), name='setLocationDB'),
+    # path("delDB/", deleteLocationDB.as_view(), name='delLocationDB'),
 ]
