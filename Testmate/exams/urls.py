162ad775156a6c05279e8da2ab2fa3ae6ab55c6f
jiwoong264 <jiwoong264@gmail.com>
2023-08-17 15:38:50 +0900

    ADD: url 변경

--- Testmate/exams/urls.py
+++ Testmate/exams/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("", ExamListView.as_view(), name='ExamList'),
-    path("detail/<str:exam_id>/", ExamDetailView.as_view(), name='ExamDetail'),
-    path("favorite/", ExamFavoriteView.as_view(), name='ExamFavorite'),
-    path("recent/", ExamRecentView.as_view(), name='ExamRecent'),
+    path("", ExamListView.as_view(), name='ExamList'), # 시험 목록 조회 [GET]
+    path("detail/<str:exam_id>/", ExamDetailView.as_view(), name='ExamDetail'), # 시험 상세 조회 [POST]
+    path("favorite/", ExamFavoriteView.as_view(), name='ExamFavorite'), # 즐겨찾기 시험 조회 [GET, POST, DELETE]
+    path("recent/", ExamRecentView.as_view(), name='ExamRecent'), # 최근조회 시험 조회 [GET]
+
 
     # path("setDB/", setExamDB.as_view(), name='setDB'),
     # path("setDBXML/", setExamDB_XML.as_view(), name='setDBXML'),
