d81687e3888bf327e9f931660c45b5ac19a8d5f5
jiwoong264 <jiwoong264@gmail.com>
2023-08-17 15:08:47 +0900

    Feat: accounts/admin.py 변경사항 반영

--- Testmate/accounts/admin.py
+++ Testmate/accounts/admin.py
@@ -1,34 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin
 from .models import User
-from .forms import UserCreationForm, UserChangeForm
-
-class CustomUserAdmin(UserAdmin):
-    # 폼 설정
-    add_form = UserCreationForm
-    form = UserChangeForm
-
-    list_display = ('user_id', 'profile_nickname', 'profile_image', 'email')
-    list_filter = ('is_superuser',)
-
-    # fieldsets = (
-    #     (None, {'fields': ('email', 'password',)}),
-    #     ('Personal info', {'fields':('user_id', 'profile_nickname', 'profile_image', 'email')}),
-    #     ('Permissions', {'fields': ('is_superuser',)}),
-    # )
-
-    # add_fieldsets = (
-    #     (None, {
-    #         'classes': ('wide',),
-    #         'fields': ('name', 'password1', 'password2')}
-    #     ),
-    # )
-    search_fields = ('email',)
-    ordering = ('user_id',)
-    filter_horizontal = ()
-
-
-
-admin.site.register(User, CustomUserAdmin)
-admin.site.unregister(Group)
+admin.site.register(User)
