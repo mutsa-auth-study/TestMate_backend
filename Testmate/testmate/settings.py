4ac40b9c16895557d7e0c7d9309904ba01103a4c
jiwoong264 <jiwoong264@gmail.com>
2023-08-17 15:51:23 +0900

    ADD: CORS 프론트 배포 주소 허용

--- Testmate/testmate/settings.py
+++ Testmate/testmate/settings.py
@@ -141,11 +141,11 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'testmate.urls'
 
 
-CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000']
+CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://testmate.vercel.app']
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_HEADERS = list(default_headers) + ['x-csrftoken']
-CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000", 'https://testmate.vercel.app']
 
 TEMPLATES = [
     {
