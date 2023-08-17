e41d090157de9257e86b6daf4d3eb92ae08d1720
jiwoong264 <jiwoong264@gmail.com>
2023-08-17 15:08:20 +0900

    Feat: accounts/views.py 변경사항 반영

--- Testmate/accounts/views.py
+++ Testmate/accounts/views.py
@@ -1,4 +1,6 @@
-from accounts.models import User
+import uuid
+import json
+from accounts.models import User, UserManager
 from allauth.socialaccount.models import SocialAccount
 from django.conf import settings
 from dj_rest_auth.registration.views import SocialLoginView
@@ -7,87 +9,89 @@ from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from django.http import JsonResponse
 import requests
 from rest_framework import status
-from json.decoder import JSONDecodeError
+from rest_framework.decorators import api_view, permission_classes
 from testmate.settings import KAKAO_REDIRECT_URI
+from rest_framework.response import Response
+from .serializers import UserInfoSerializer
+from rest_framework import permissions
+
+
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # 필요한 모듈 및 클래스를 가져옵니다
 BASE_URL = 'http://localhost:8000/'  # 애플리케이션의 기본 URL을 정의합니다
-KAKAO_CALLBACK_URI = BASE_URL + KAKAO_REDIRECT_URI
-
+KAKAO_CALLBACK_URI = KAKAO_REDIRECT_URI
+@permission_classes ([permissions.AllowAny])
+@api_view(['POST'])
 def kakao_callback(request):
     """
     프론트엔드에서 액세스 토큰을 가져옵니다
     """
-    # 'Bearer: ' 접두어를 제거하여 액세스 토큰을 추출합니다
-    access_token = request.headers.get('Authorization')[7:]
-
+    # 'Bearer: ' 접두어를 제거하여 클라이언트로부터 받은 액세스 토큰을 추출합니다.
+    access_token = json.loads(request.body)['Authorization']
     """
-    이메일 요청
+    카카오 API로 사용자의 이메일 정보를 요청
     """
     email_req = requests.get(
-        "https://kapi.kakao.com/v2/user/me", headers={"Authorization": f"Bearer {access_token}"})
+        "https://kapi.kakao.com/v2/user/me", headers={"Authorization": access_token})
     email_req_status = email_req.status_code
+    # 이메일 정보를 성공적으로 받아왔는지 확인
     if email_req_status != 200:
         return JsonResponse({'err_msg': '이메일을 가져오는데 실패했습니다'}, status=status.HTTP_400_BAD_REQUEST)
     email_req_json = email_req.json()
-    userName = email_req_json.get('username')
-    user_id = email_req_json.get('user_id')
-    profile_nickname = email_req_json.get('email')
-    # email = email_req_json.get('email')
-    userInfo = {}
-    userInfo["user_id"] = uuid.uuid()
-    userInfo["username"] = userName
-    userInfo["kakao_id"] = kakao_id
-    userInfo["profile_nickname"] = profile_nickname
-    userInfo["username"] = userName
+    email = email_req_json.get('email') # 이메일
+
+    # 사용자 정보를 가공
+    kakao_account = email_req_json.get('kakao_account', {})  # 기본값으로 빈 딕셔너리 사용
+    profile = kakao_account.get('profile', {})
+
+    kakao_id = email_req_json.get('id')  # 카카오 아이디
+    email = kakao_account.get('email')  # 이메일
+    profile_nickname = profile.get('nickname')  # 닉네임
+    profile_image = profile.get('thumbnail_image_url')  # 프로필 사진
+
 
     """
     가입 또는 로그인 요청
     """
     try:
-        # 기존에 가입된 유저의 Provider가 kakao가 아니면 에러 발생, 맞으면 로그인
-        user = User.objects.get(email=email)
-        data = {'access_token': access_token}
-        accept = requests.post(
-            f"{BASE_URL}accounts/kakao/login/finish/", data=data)
-        accept_status = accept.status_code
-        if accept_status != 200:
-            return JsonResponse({'err_msg': '로그인에 실패했습니다'}, status=accept_status)
-        accept_json = accept.json()
+        # 1. 유저 모델, 유저 매니저에서 유저 정보 가져오는 함수 (get(email=email))
+        user = User.objects.get(email = email)
+        if user is None: raise
+        # 2. 해당 정보로 access token, refresh token 발급
+        refresh = RefreshToken.for_user(user)
+
+        # 3. return Response()
+        
+        # Add the 'accessToken' field to the user_info dictionary
+        
+
+        # Serialize the user info using the extended serializer
+        serializer = UserInfoSerializer(instance=user)
+        response_body = serializer.data
+        response_body['user_id'] = response_body.pop('pk')
+        response_body['accessToken'] = str(refresh.access_token) # Replace with the actual access token
+        return Response({'information':response_body}, status= status.HTTP_201_CREATED)
 
     except:
-        # 기존에 가입된 유저가 없으면 새로 가입
-        data = {'access_token': access_token}
-        accept = requests.post(
-            f"{BASE_URL}accounts/kakao/login/finish/", data=data)
-        accept_status = accept.status_code
-        if accept_status != 200:
-            return JsonResponse({'err_msg': '가입에 실패했습니다'}, status=accept_status)
-        accept_json = accept.json()
-
-        #
-        serializer = UserInfolSerializer(data=userInfo)
-        if serializer.is_valid():
-            serializer.save()  # 데이터베이스에 저장
-
-    user = User.objects.get(email=email)
-
-    
-    accept_json['user_id'] = user.user_id
-    accept_json['profile_nickname'] = user.profile_nickname
-    accept_json['profile_image'] = user.profile_image.url if user.profile_image.url else None
-    accept_json['email'] = user.email
-    accept_json['accessToken'] = user.accessToken
-
-    accept_json.pop('user', None)
-
-    response_data = {
-        "status": status.HTTP_200_OK,
-        "information": [accept_json]
+        # 1. 유저 모델, 유저 매니저에서 유저 정보 생성하는 함수 ( create_user(email= "", ... etc))
+        extra_fields = {
+        'kakao_id': kakao_id,
+        'profile_nickname': profile_nickname,
+        'profile_image': profile_image,
         }
+        user = User.objects.create_user(email=email, password='', **extra_fields)
 
-    return JsonResponse(response_data)
+        # 2. 해당 정보로 access token, refresh token 발급
+        refresh = RefreshToken. for_user(user)
 
+        # 3. return Response()
+        # Serialize the user info using the extended serializer
+        serializer = UserInfoSerializer(user)
+        response_body = serializer.data
+        response_body['user_id'] = response_body.pop('pk')
+        response_body['accessToken'] = str(refresh.access_token) # Replace with the actual access token
+        return Response({'information':response_body}, status= status.HTTP_201_CREATED)
 
 class KakaoLogin(SocialLoginView):
     adapter_class = kakao_view.KakaoOAuth2Adapter
