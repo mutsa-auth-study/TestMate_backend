d22b1a9807f8488707eabf87a5b927f16df4f710
jiwoong264 <jiwoong264@gmail.com>
2023-08-18 21:10:51 +0900

    Fix: 메인페이지 정보 수정, 최근조회 중복처리

--- Testmate/exams/views.py
+++ Testmate/exams/views.py
@@ -20,7 +20,6 @@ lock = Lock()
 
 # 메인 화면 API
 class ExamMainView(APIView):
-    #permission_classes = [AllowAny]
     
     def get(self, request):
 
@@ -45,9 +44,8 @@ class ExamMainView(APIView):
         examsList = data1.values()
         examsPlanList = data2.values()
     
-        for exam in examsList:
-            for examPlan in examsPlanList:
-                exam.update(examPlan)
+        for i in range(len(examsList)):
+            examsList[i].update(examsPlanList)
 
         response_data = {
             "status": status.HTTP_200_OK,
@@ -55,29 +53,6 @@ class ExamMainView(APIView):
         }
         return Response(response_data, status=status.HTTP_200_OK)
 
-'''
-        data = Exam.objects.all()
-        exam_list = list(data.values())  # 시험 정보를 담을 리스트 초기화
-        
-        if request.user.is_authenticated:
-            # 로그인한 사용자일 경우, 즐겨찾기한 시험 ID들을 가져와 리스트로 변환
-            exam_favorites = ExamFavorite.objects.filter(user=request.user.id)
-            favorite_exam_ids = [exam_favorite.exam_id for exam_favorite in exam_favorites]
-    
-            # 즐찾 여부 확인
-            for exam in exam_list:
-                if exam.exam_id in favorite_exam_ids:
-                    exam["is_favorite"] = True
-                else:
-                    exam["is_favorite"] = False
-            # 미로그인 사용자는 모두 즐찾X
-        else:
-            return Response(status=status.HTTP_401_UNAUTHORIZED)
-        #     for exam in exam_list:
-        #         exam["is_favorite"] = False
-
-        # # 시험들의 리스트 반환
-        '''
 
 # 시험 목록 조회
 class ExamListView(APIView):
@@ -113,15 +88,6 @@ class ExamListView(APIView):
         return Response(response_data, status=status.HTTP_200_OK)
 
 
-        
-        # 페이지네이션 적용
-        # 일단 주석처리 해놓겠음.
-        paginator = CustomPageNumberPagination()
-        paginated = paginator.paginate_queryset(Exam,request)
-        serializer = ExamTotalSerializer(paginated, many=True)
-        print(paginator.get_paginated_response(serializer.data))
-        return paginator.get_paginated_response(serializer.data)
-  
 class ExamDetailView(APIView):
 
     decodedKey = "WKylCY9PiFAjyG1rstW8XGqQbs7lkyQWXRGIpZDC5RNJnSdK9W0BaUJF5KPRI6Y2e2VsiB9loeLTG/+8nJcLHw=="
@@ -132,35 +98,43 @@ class ExamDetailView(APIView):
     def post(self, request, *args, **kwargs):
         examID = kwargs.get('exam_id')
         # 로그인 된 경우 최근 본 시험 데이터 등록
-        if request.user.is_authenticated:
-            userID = request.user.id
-            recent_data = {"user_id":userID, "exam_id":examID}
-            print(recent_data)
-            # 새로운 조회 정보를 저장
-            serializer = ExamRecentSerializer(data=recent_data)
-            print("시리얼라이저 : ",serializer)
-            if serializer.is_valid():
-                serializer.save()  # 데이터베이스에 저장
-                print("최근 조회 등록 성공")
-            else:
-                print(serializer.is_valid())
-                print("최근 조회 등록 실패")
+        try :
+            if request.user.is_authenticated:
+                userID = request.user.id
                 
-            ''''
-            # 현재 저장된 조회 정보의 개수 체크
-            count = ExamRecent.objects.filter(user_id=userID).count()
+                # 이미 봤으면 패스
+                exam = ExamRecent.objects.get(user_id = userID, exam_id = examID)
+                if exam is None: raise
 
-            # 10개 초과하는 경우 가장 오래된 정보 삭제
-            if count > 10:
-                oldest_exam = ExamRecent.objects.filter(user_id=userID).earliest("recent_id")
-                oldest_exam.delete()
-                print("10개 초과 삭제")
-            '''
+                recent_data = {"user_id":userID, "exam_id":examID}
+                # 새로운 조회 정보를 저장
+                serializer = ExamRecentSerializer(data=recent_data)
+                
+                if serializer.is_valid():
+                    serializer.save()  # 데이터베이스에 저장
+                    print("최근 조회 등록 성공")
+                else:
+                    print("최근 조회 등록 실패")
+                    
+                
+                # 현재 저장된 조회 정보의 개수 체크
+                count = ExamRecent.objects.filter(user_id=userID).count()
 
-        # DB에서 해당 시험 일정 저장된 것 있는지 확인
+                # 10개 초과하는 경우 가장 오래된 정보 삭제
+                if count > 10:
+                    oldest_exam = ExamRecent.objects.filter(user_id=userID).earliest("recent_id")
+                    oldest_exam.delete()
+                    print("10개 초과 삭제")
+                
+
+            # DB에서 해당 시험 일정 저장된 것 있는지 확인
+        except exam.DoesNotExist:
+            print("이미 조회한 시험")
+
+        # DB에 있으면 꺼내서 호출
         db = ExamPlan.objects.filter(exam_id=examID)
         if len(db):
-            print("중복")
+            print("디비에 저장된 일정")
             response_data = {
                 "status": status.HTTP_200_OK,
                 "information": list(db.values())
@@ -183,65 +157,25 @@ class ExamDetailView(APIView):
         # plan = []
         for item in root.findall('.//item'):
             dict = {}
-            implYy = item.find('implYy').text
-            if implYy == None:
-                implYy = " "
-            implSeq = item.find('implSeq').text
-            if implSeq == None:
-                implSeq = " "
-            description = item.find('description').text
-            if description == None:
-                description = " "
-            docRegStartDt = item.find('docRegStartDt').text
-            if docRegStartDt == None:
-                docRegStartDt = " "
-            docRegEndDt = item.find('docRegEndDt').text
-            if docRegEndDt == None:
-                docRegEndDt = " "
-            docExamStartDt = item.find('docExamStartDt').text
-            if docExamStartDt == None:
-                docExamStartDt= " "
-            docExamEndDt = item.find('docExamEndDt').text
-            if docExamEndDt == None:
-                docExamEndDt = " "
-            docPassDt = item.find('docPassDt').text
-            if docPassDt == None:
-                docPassDt = " "
-            pracRegStartDt = item.find('pracRegStartDt').text
-            if pracRegStartDt == None:
-                docPassDt= " "
-            pracRegEndDt = item.find('pracRegEndDt').text
-            if pracRegEndDt == None:
-                docPassDt = " "
-            pracExamStartDt = item.find('pracExamStartDt').text
-            if pracExamStartDt == None:
-                pracExamStartDt = " "
-            pracExamEndDt = item.find('pracExamEndDt').text
-            if pracExamEndDt == None:
-                pracExamEndDt = " "
-            pracPassDt = item.find('pracPassDt').text
-            if pracPassDt == None:
-                pracPassDt = " "
-            
-            dict["implYy"] = implYy
-            dict["implSeq"] = implSeq
-            dict["description"] = description
-            dict["docRegStartDt"] = docRegStartDt
-            dict["docRegEndDt"] = docRegEndDt
-            dict["docExamStartDt"] = docExamStartDt
-            dict["docExamEndDt"] = docExamEndDt
-            dict["docPassDt"] = docPassDt
-            dict["pracRegStartDt"] = pracRegStartDt
-            dict["pracRegEndDt"] = pracRegEndDt
-            dict["pracExamStartDt"] = pracExamStartDt
-            dict["pracExamEndDt"] = pracExamEndDt
-            dict["pracPassDt"] = pracPassDt
+
+            dict["implYy"] = item.find('implYy').text
+            dict["implSeq"] = item.find('implSeq').text
+            dict["description"] = item.find('description').text
+            dict["docRegStartDt"] = item.find('docRegStartDt').text
+            dict["docRegEndDt"] = item.find('docRegEndDt').text
+            dict["docExamStartDt"] = item.find('docExamStartDt').text
+            dict["docExamEndDt"] = item.find('docExamEndDt').text
+            dict["docPassDt"] = item.find('docPassDt').text
+            dict["pracRegStartDt"] = item.find('pracRegStartDt').text
+            dict["pracRegEndDt"] = item.find('pracRegEndDt').text
+            dict["pracExamStartDt"] = item.find('pracExamStartDt').text
+            dict["pracExamEndDt"] = item.find('pracExamEndDt').text
+            dict["pracPassDt"] = item.find('pracPassDt').text
             
             dict["exam_id"] = examID
 
             serializer = ExamDetailSerializer(data=dict)
-            print(serializer)
-            print(serializer.is_valid)
+            
             if serializer.is_valid():
                 serializer.save()  # 데이터베이스에 저장
                 response_data = {
@@ -257,32 +191,7 @@ class ExamDetailView(APIView):
         #         "information": plan
         #     }
         # return Response(response_data, status=status.HTTP_200_OK)
-        
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-'''
-    # 사용자가 시험을 조회했을 때, 그 정보 DB에 저장하는 로직
-    # 이때, 개수를 체크하고 10개 초과 시 가장 오래된 정보 삭제
-
-    def post(self, request):
-        user_id = request.user.id
-        exam_id = request.data.get('exam_id')
-
-        # 새로운 조회 정보를 저장
-        recent_exam = ExamRecent(user_id=user_id, exam_id=exam_id)
-        recent_exam.save()
-
-        # 현재 저장된 조회 정보의 개수 체크
-        count = ExamRecent.objects.filter(user_id=user_id).count()
-
-        # 10개 초과하는 경우 가장 오래된 정보 삭제
-        if count > 10:
-            oldest_exam = ExamRecent.objects.filter(user_id=user_id).earliest("recent_id")
-            oldest_exam.delete()
-
-        return Response({"message:": "Success"}, status=status.HTTP_201_CREATED)
-'''
-    
+            
 
 # 즐겨찾기
 class ExamFavoriteView(APIView):
@@ -323,15 +232,6 @@ class ExamFavoriteView(APIView):
         }
         return Response(response_data, status=status.HTTP_200_OK)
 
-        # # 시리얼라이징
-        # serializer = ExamTotalSerializer(exams, many=True)
-
-        # # 반환
-        # response_data = {
-        #             "status": status.HTTP_200_OK,
-        #             "information": serializer.data
-        #         }
-        # return Response(response_data, status=status.HTTP_200_OK)
 
     # 즐겨찾기 등록
     def post(self, request):
@@ -344,9 +244,9 @@ class ExamFavoriteView(APIView):
 
             # 현재 즐겨찾기한 시험 ID 개수 확인
             favorite_count = ExamFavorite.objects.filter(user_id=user_id).count()
-            # 즐겨찾기한 시험 개수가 10개 이상이면 실패 응답 반환
-            if favorite_count >= 10:
-                return Response({"detail": "You can only add up to 10 exams to favorites."}, status=status.HTTP_400_BAD_REQUEST)
+            # 즐겨찾기한 시험 개수가 5개 이상이면 실패 응답 반환
+            if favorite_count >= 5:
+                return Response({"detail": "You can only add up to 5 exams to favorites."}, status=status.HTTP_400_BAD_REQUEST)
 
             # 요청 데이터에 exam_id가 없으면 실패 응답 반환
             if not exam_id:
@@ -409,7 +309,7 @@ class ExamRecentView(APIView):
         '''
         # 최적화
         # Exam모델에서 favorite_exam_ids에 있는 exam_id와 일치하는 시험 정보 필드 가져오기
-        exams = Exam.objects.filter(exams_id__in=recent_exams_ids)
+        exams = Exam.objects.filter(exam_id__in=recent_exams_ids)
 
         # 시리얼라이징
         serializer = ExamTotalSerializer(exams, many=True)
